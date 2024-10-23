6a61a29af8a8cefaf0c4f2951e576879d58aa473
David Ng <david.ng.dev@gmail.com>
2024-10-24 00:11:14 +0100

    integrated with API

--- crawler.js
+++ crawler.js
@@ -1,70 +1,57 @@
-import { PlaywrightCrawler, Dataset } from 'crawlee';
-import { createPlaywrightRouter } from 'crawlee';
+import {BrowserPool, PlaywrightPlugin } from '@crawlee/browser-pool';
+import playwright from 'playwright';
 
-const router = createPlaywrightRouter()
-
-const playwrightCrawler = new PlaywrightCrawler({
-    requestHandler: router,
-})
+export const browserPool = new BrowserPool({
+    browserPlugins: [new PlaywrightPlugin(playwright.chromium)],
+    preLaunchHooks: [() => {
+        console.log('Pre-launch browser...')
+    }],
+});
 
 export const crawler = {
     async run(requestId, params) {
-        const taskStatistics = await playwrightCrawler.run(params)
-        if (taskStatistics.requestsFailed !== 0) throw new Error("Unknown Error!")
-        const store = await Dataset.open(requestId)
-        const resp = await store.getData()
-        await store.drop()
-        return resp.items[0];
-    }
-}
-
-router.addHandler("OCADO", async ({ request, page, log }) => {
-    const requestId = request.userData['requestId']
-    log.info(`Request running on ${requestId}`)
-    const store = await Dataset.open(requestId)
-    const productOpenGraphMeta = Object.fromEntries(
-        await page
-            .locator('meta[property^="og:"]')
-            .evaluateAll(elements =>
-                elements.map(ele => [
-                    ele.getAttribute('property'),
-                    ele.getAttribute('content'),
-                ]),
-            ),
-    );
-    const productId = new URL(productOpenGraphMeta['og:url'], request.url).pathname
-        .split('/')
-        .pop();
-    const priceInfo = Object.fromEntries(
-        (
+        const param = params[0];
+        const page = await browserPool.newPage();
+        await page.goto(param.url);
+        const productOpenGraphMeta = Object.fromEntries(
             await page
-                .locator('meta[itemprop]')
+                .locator('meta[property^="og:"]')
                 .evaluateAll(elements =>
                     elements.map(ele => [
-                        ele.getAttribute('itemprop'),
+                        ele.getAttribute('property'),
                         ele.getAttribute('content'),
                     ]),
-                )
-        ).filter((entity) =>['price', 'priceCurrency'].includes(String(entity[0]))));
-    const pricePerItem = (await page.locator('.bop-price__per').isVisible())
-        ? await page.locator('.bop-price__per').textContent()
-        : null;
+                ),
+        );
+        const productId = new URL(productOpenGraphMeta['og:url'], param.url).pathname
+            .split('/')
+            .pop();
+        const priceInfo = Object.fromEntries(
+            (
+                await page
+                    .locator('meta[itemprop]')
+                    .evaluateAll(elements =>
+                        elements.map(ele => [
+                            ele.getAttribute('itemprop'),
+                            ele.getAttribute('content'),
+                        ]),
+                    )
+            ).filter((entity) =>['price', 'priceCurrency'].includes(String(entity[0]))));
+        const pricePerItem = (await page.locator('.bop-price__per').isVisible())
+            ? await page.locator('.bop-price__per').textContent()
+            : null;
 
-    const price = new Intl.NumberFormat('en-GB', {
-        currency: priceInfo.priceCurrency,
-        style: 'currency',
-    }).format(Number(priceInfo.price));
-    store.pushData({
-        price,
-        pricePerItem,
-        productId,
-        ogProps: productOpenGraphMeta
-    })
-    console.log({
-        price,
-        pricePerItem,
-        productId,
-        ogProps: productOpenGraphMeta
-    })
-
-})
+        const price = new Intl.NumberFormat('en-GB', {
+            currency: priceInfo.priceCurrency,
+            style: 'currency',
+        }).format(Number(priceInfo.price));
+        await page.close()
+        return {
+            requestId,
+            price,
+            pricePerItem,
+            productId,
+            ogProps: productOpenGraphMeta
+        }
+    }
+}
--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-import {crawler} from "./crawler.js"
+import {crawler, browserPool} from "./crawler.js"
+import {createServer} from "node:http"
 
-const requestId = crypto.randomUUID()
-// Start the crawler with the provided URLs
-const resp = await crawler.run(requestId, [{
+const server = createServer(async (req, res) => {
+    const reqId = req.headers['request-id']
+    if (!reqId) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Request ID is required')
+        return;
+    }
+    const resp = await crawler.run(reqId, [{
     userData: {
         label: 'OCADO',
-        requestId
+        requestId: reqId
     },
     url: 'https://www.ocado.com/products/asahi-super-dry-beer-lager-bottles-631029011'
 }])
-
-console.log(`resp = `, resp)
-
-// Start the crawler with the provided URLs
-// await crawler.run([{
-//     userData: {
-//         label: 'OCADO'
-//     },
-//     url: 'https://www.ocado.com/products/asahi-super-dry-beer-lager-bottles-631029011'
-// }]);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(Object.assign(resp, {reqId})));
+})
+server.listen(3000, () => {
+    console.log('Server listening on port 3000')
+    Promise.all([
+        'b8367ade-9544-4ab5-887f-4a043ed7d442',
+        'f37d1463-8785-4cde-b38b-235e79070783',
+        'fba8e239-fc62-475b-929f-affe55c92238',
+        '02195a79-b5cb-4825-8f3f-292b480bb27b',
+        '53dc6c46-e91b-43ce-8a9c-c45f2734ce15',
+        '3961e622-8609-4cf5-9b4e-248d5dd99b8d',
+        '5922bff5-da03-46c3-9b5c-669ad487b098',
+        'bf58c058-1d73-4333-8ec4-49af5aaeacf2'
+    ]
+        .map(async (requestId) => {
+            console.time(requestId);
+            const resp = await fetch('http://localhost:3000', {
+                headers: {
+                    'request-id': requestId
+                }
+            }).then(resp => resp.json())
+            console.timeEnd(requestId);
+            return resp
+    }))
+        .finally(async () => {
+            await browserPool.destroy()
+            server.close()
+        })
+})
